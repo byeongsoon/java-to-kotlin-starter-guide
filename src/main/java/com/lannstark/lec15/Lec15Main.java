5502426f20f051f4b08ba9c777cf7a45490d9ca1
byeongsoon <byeongsoon94@gmail.com>
2023-02-08 19:58:00 +0900

    Lec15. 코틀린에서 배열과 컬렉션을 다루는 방법

--- src/main/java/com/lannstark/lec15/Lec15Main.java
+++ src/main/java/com/lannstark/lec15/Lec15Main.java
@@ -3,6 +3,11 @@ package com.lannstark.lec15;
 public class Lec15Main {
 
   public static void main(String[] args) {
+    int[] array = {100, 200};
+
+    for (int i = 0; i < array.length; i++) {
+      System.out.printf("%s %s", i, array[i]);
+    }
   }
 
 }
